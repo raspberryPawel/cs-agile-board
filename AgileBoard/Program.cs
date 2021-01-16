7dbf01488009d00c3b48dd13495faaa7dc2cd211
Pawel Malina <pawel.malina1999@gmail.com>
2021-01-16 16:09:22 +0100

    initial commit

---
+++ AgileBoard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AgileBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
