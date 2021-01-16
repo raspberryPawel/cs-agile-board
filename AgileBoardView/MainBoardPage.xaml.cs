b198c4fcb8b059b2065f9a464602972e42e823e3
Pawel Malina <pawel.malina1999@gmail.com>
2021-01-16 17:02:04 +0100

    added navigation and poc board page

---
+++ AgileBoardView/MainBoardPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AgileBoardView
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainBoardPage.xaml
+    /// </summary>
+    public partial class MainBoardPage : Page
+    {
+        public MainBoardPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
