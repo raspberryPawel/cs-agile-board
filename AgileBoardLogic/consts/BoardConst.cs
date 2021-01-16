4377b30c2c2f953b295060a0f94e8125db86e3ee
Pawel Malina <pawel.malina1999@gmail.com>
2021-01-16 16:48:20 +0100

    added wpf project

---
+++ AgileBoardLogic/consts/BoardConst.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace AgileBoardLogic
+{
+    class BoardConst
+    {
+        public enum BoardColumns { Open, Coding, Test, Resolve };
+        public static Dictionary<BoardColumns, string> BoardColumnsNames = new Dictionary<BoardColumns, string>()
+        {
+            [BoardColumns.Open] = "Open",
+            [BoardColumns.Coding] = "Coding",
+            [BoardColumns.Test] = "Test",
+            [BoardColumns.Resolve] = "Resolve",
+        };
+    }
+}
